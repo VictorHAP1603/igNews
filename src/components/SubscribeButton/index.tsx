a822eecf97624788195051dcaa8dde8738440109
VictorHugo1603 <vitor.hugo@newpost.com>
2021-09-21 22:43:02 -0300

    adicionando eventos webhooks stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,43 @@
+import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./style.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+// Seguros ===
+// ==== getServerSideProps
+// ==== getStaticProps
+// ==== ApiRoutes
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {  
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    // verifica se ta logado
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    // criação da checkout session;
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({sessionId});
+
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe Now
     </button>
   );
