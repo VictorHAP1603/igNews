02aa0f0fb9ff17fa89a527c42f081d5691eca245
VictorHugo1603 <vitor.hugo@newpost.com>
2021-09-24 21:07:25 -0300

    validando subscription

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,30 @@
 // import
-// import Image from 'next/image'
+import LogoImg from '../../assets/images/logo.svg';
+import Image from "next/image";
+import Link from 'next/link'
 
 import styles from "./style.module.scss";
-
 import { SignInButton } from "../SignInButton";
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="Logo IgNews" />
+        <Image src={LogoImg} alt="Logo IgNews" />
 
         <nav>
-          <a href="" className={styles.active}>
+          <ActiveLink activeClassName={styles.active} href="/" >
+            <a>
             Home
-          </a>
-          <a href="">Posts</a>
+            </a>
+          </ActiveLink>
+          
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
